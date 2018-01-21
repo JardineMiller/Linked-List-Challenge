a0ba81b042a12c229974f1d6edd9ca0c8f8a786e
Jardine <jardine.miller@gmail.com>
2018-01-21 18:17:54 +0000

    can addEnd and removeEnd & get(index) added

--- src/org/list/LinkedList.java
+++ src/org/list/LinkedList.java
@@ -1,4 +1,67 @@
 package org.list;
 
+class Node {
+    int value;
+    Node next;
+
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
 public class LinkedList {
+    private int numOfElements = 0;
+    private Node head = null;
+
+    public int size() {
+        return numOfElements;
+    }
+
+    public int getHead() {
+        return head.value;
+    }
+
+    public void insertToFront(int i) {
+         Node newNode  = new Node(i);
+         newNode.next = head;
+
+         this.head = newNode;
+         this.numOfElements++;
+    }
+
+    public void removeFromFront() {
+        this.head = head.next;
+        this.numOfElements--;
+    }
+
+    public Node get(int index) {
+        Node node = this.head;
+        for(int i = 0; i <= index - 1; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    public void insertToEnd(int i) {
+        if(numOfElements > 0) {
+            Node newNode = new Node(i);
+            Node lastNode = this.get(this.numOfElements - 1);
+            lastNode.next = newNode;
+            numOfElements++;
+        } else {
+            Node newNode  = new Node(i);
+            newNode.next = head;
+
+            this.head = newNode;
+            this.numOfElements++;
+        }
+    }
+
+    public void removeFromEnd() {
+        Node penultimateNode = this.get(numOfElements - 2);
+        penultimateNode.next = null;
+        numOfElements--;
+    }
+
 }
--- test/org/list/LinkedListTest.java
+++ test/org/list/LinkedListTest.java
@@ -1,13 +1,78 @@
 package org.list;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class LinkedListTest {
+    LinkedList list;
+    Node node;
+
+    @Before
+    public void before() {
+        list = new LinkedList();
+    }
 
     @Test
     public void WhenCreated_LinkedListHasNoElements() {
-
+        assertEquals(0, list.size());
     }
+
+    @Test
+    public void canGetHead() {
+        assertEquals(null, list.getHead());
+    }
+
+    @Test
+    public void canInsertToFront() {
+        list.insertToFront(99);
+        assertEquals(1, list.size());
+        assertEquals(99, list.getHead());
+    }
+
+    @Test
+    public void canRemoveFromFront() {
+        list.insertToFront(5);
+        list.insertToFront(15);
+        list.removeFromFront();
+        assertEquals(1, list.size());
+        assertEquals(5, list.getHead());
+    }
+
+    @Test
+    public void canGet() {
+        list.insertToFront(5);
+        list.insertToFront(15);
+        list.insertToFront(30);
+        assertEquals(30, list.get(0).value);
+        assertEquals(15, list.get(1).value);
+        assertEquals(5, list.get(2).value);
+    }
+
+    @Test
+    public void canAddToEnd() {
+        list.insertToFront(5);
+        list.insertToFront(15);
+        list.insertToFront(30);
+        list.insertToEnd(100);
+        assertEquals(30, list.get(0).value);
+        assertEquals(15, list.get(1).value);
+        assertEquals(5, list.get(2).value);
+        assertEquals(100, list.get(3).value);
+        assertEquals(4, list.size());
+    }
+
+    @Test
+    public void canRemoveFromEnd() {
+        list.insertToEnd(5);
+        list.insertToEnd(15);
+        list.insertToEnd(100);
+        list.removeFromEnd();
+        assertEquals(2, list.size());
+        assertEquals(15, list.get(1).value);
+    }
+
 
 
 }
